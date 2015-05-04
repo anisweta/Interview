e2998b6ad90b8fb04c2e6784acfbe102022e9415
anisweta <anisweta@gmail.com>
2015-05-04 12:59:09 -0700

    updating node class
    added createNode method

--- src/com/interview/linkedlist/Node.java
+++ src/com/interview/linkedlist/Node.java
@@ -1,6 +1,19 @@
 package com.interview.linkedlist;
 
+/**
+ * 
+ * @author Anisweta
+ * Purpose:This class consists of basic structure of a node - data and next(address of next node).
+ * Methods: It has a static method creatNode which helps in creating node.
+ */
 public class Node {
 	public int data;
 	public Node next;
+	
+	public static Node createNode(int data){
+		Node node = new Node();
+		node.data=data;
+		node.next=null;
+		return node;
+	}
 }
